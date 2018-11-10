2d0239ec188290bf0d1f4bca1ea418b849f2a658
Aaron Pennington <b.aaron.pennington@gmail.com>
2018-11-09 22:39:55 -0600

    Added a static gui, removed some debug functions

--- weather.py
+++ weather.py
@@ -6,6 +6,9 @@ import time
 import requests
 import json
 import math
+import sys
+from PyQt5 import QtWidgets
+from mainwindow import Ui_MainWindow
 
 class Weather():
     def __init__(self):
@@ -161,6 +164,7 @@ class Weather():
         time_current = time.time()
         file_time.close()
 
+
         # NOTE: This statement may be obselete. Okay, it __is__ obsolete. All the time checking
         # functions here can be removed. I'm just too scared to do so. 
         if time_current - time_last_call < 660:
@@ -180,6 +184,7 @@ class Weather():
             temp_kel = self.read_weather(res)
             temp_fah = self.convert_temp(temp_kel)
             print("Temperature: " + str(temp_fah))
+            return temp_fah
         elif type == "forecast":
             temp_kel = self.read_forecast(res)
 
@@ -199,7 +204,24 @@ class Weather():
         self.check_time(t)
 
 
-weather = Weather()
-weather.parse_command(weather.get_command())
+class ApplicationWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(ApplicationWindow, self).__init__()
 
-input("Press ENTER to exit.")
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+def main():
+    weather = Weather()
+    app = QtWidgets.QApplication(sys.argv)
+    application = ApplicationWindow()
+    application.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
+
+
+#input("Press ENTER to exit.")
